e795c7d3db1914d2465d54c086c46b107b75c422
Idris Bowman <34751375+V00D00-child@users.noreply.github.com>
2023-07-05 17:14:48 -0400

    add-aa

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,18 @@
-// This is a script for deploying your contracts. You can adapt it to deploy
-// yours, or create new ones.
 
+const { network } = require("hardhat");
 const path = require("path");
 
 async function main() {
-  // This is just a convenience check
-  if (network.name === "hardhat") {
-    console.warn(
-      "You are trying to deploy a contract to the Hardhat Network, which" +
-        "gets automatically created and destroyed every time. Use the Hardhat" +
-        " option '--network localhost'"
-    );
+  if (network.config.chainId !== 1337 && network.config.chainId !== 31337) {
+    console.log('NOT deploying contracts. use pre-deployed contracts')
+    process.exit(1)
   }
 
-  // ethers is available in the global scope
   const [deployer] = await ethers.getSigners();
   console.log(
     "Deploying the contracts with the account:",
     await deployer.getAddress()
   );
-
-  console.log("Account balance:", (await deployer.getBalance()).toString());
 
   const Token = await ethers.getContractFactory("Token");
   const token = await Token.deploy();
@@ -34,7 +26,7 @@ async function main() {
 
 function saveFrontendFiles(token) {
   const fs = require("fs");
-  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts", ``);
 
   if (!fs.existsSync(contractsDir)) {
     fs.mkdirSync(contractsDir);
--- tasks/faucet.js
+++ tasks/faucet.js
@@ -1,7 +1,4 @@
-const fs = require("fs");
-
-// This file is only here to make interacting with the Dapp easier,
-// feel free to ignore it if you don't need it.
+// This file is only here to make fund accounts easier.
 
 task("faucet", "Sends ETH and tokens to an address")
   .addPositionalParam("receiver", "The address that will receive them")
@@ -14,27 +11,7 @@ task("faucet", "Sends ETH and tokens to an address")
       );
     }
 
-    const addressesFile =
-      __dirname + "/../frontend/src/contracts/contract-address.json";
-
-    if (!fs.existsSync(addressesFile)) {
-      console.error("You need to deploy your contract first");
-      return;
-    }
-
-    const addressJson = fs.readFileSync(addressesFile);
-    const address = JSON.parse(addressJson);
-
-    if ((await ethers.provider.getCode(address.Token)) === "0x") {
-      console.error("You need to deploy your contract first");
-      return;
-    }
-
-    const token = await ethers.getContractAt("Token", address.Token);
     const [sender] = await ethers.getSigners();
-
-    const tx = await token.transfer(receiver, 100);
-    await tx.wait();
 
     const tx2 = await sender.sendTransaction({
       to: receiver,
@@ -42,5 +19,5 @@ task("faucet", "Sends ETH and tokens to an address")
     });
     await tx2.wait();
 
-    console.log(`Transferred 1 ETH and 100 tokens to ${receiver}`);
+    console.log(`Transferred 1 ETH to ${receiver}`);
   });
